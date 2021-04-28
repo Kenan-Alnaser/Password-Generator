a557b132702c2286832feb96bd561f8ea79e1271
Kenan Alnaser <kenan-na@live.com>
2021-04-28 11:12:28 +0200

    Updates

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+function generatePassword(passwordLength) {
+  let numberChars = "0123456789";
+  let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let lowerChars = "abcdefghijklmnopqrstuvwxyz";
+  let specialChars = "!§$%&?_@";
+  let allChars = numberChars + upperChars + lowerChars + specialChars;
+  let randPasswordArray = Array(passwordLength);
+  randPasswordArray[0] = numberChars;
+  randPasswordArray[1] = upperChars;
+  randPasswordArray[2] = lowerChars;
+  randPasswordArray[3] = specialChars;
+  randPasswordArray = randPasswordArray.fill(allChars, 3);
+  return shuffleArray(
+    randPasswordArray.map(function (x) {
+      return x[Math.floor(Math.random() * x.length)];
+    })
+  ).join("");
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
